3e90501064b046d1d2c8f1696f3ffbd5f7f7db07
Pedro Costa <pedrohzcosta06@gmail.com>
2024-11-15 00:17:23 -0300

    Deploy no Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './public', // Pasta onde o arquivo HTML está
+  root: './public', // Definindo a pasta 'public' como a raiz
   build: {
-    outDir: '../dist', // A pasta de saída do build
+    outDir: '../dist', // Pasta de saída do build
+    rollupOptions: {
+      input: {
+        main: './public/index.html', // Adicionando o 'index.html'
+        recursos: './public/recursos.html', // Adicionando 'recursos.html'
+        contato: './public/contato.html', // Adicionando 'contato.html'
+        blog: './public/blog.html', // Adicionando 'contato.html'
+        sobrenos: './public/sobrenos.html' // Adicionando 'contato.html'
+      }
+    }
   },
-  base: '/', // Base da URL, ajuste se necessário
+  base: '/', // Isso vai garantir que a base da URL seja relativa
 });
